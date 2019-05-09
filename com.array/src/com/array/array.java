5b7acb476ca41404ec38d21001b16994d1b31476
chikki <chikki@priyanka.fios-router.home>
2019-05-08 20:43:14 -0400

    array test code

---
+++ com.array/src/com/array/array.java
@@ -0,0 +1,25 @@
+package com.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class array {
+	public static void main(String[] args) {
+		//public static void main(String args[]){
+
+   // String [] Name = new String[5];
+		String[] Name= {"A","B","C","D"};
+    	System.out.println(Arrays.toString(Name)); //Arrays.toSTring is used to print elements in an array
+    	
+    	/*String[] name = new String[1]; // Simple array declaration
+    	int[] id = new int[1];
+    	
+    	List<String> Names = new ArrayList<String>(); //Array List Declaration
+    	List<Integer> idCard= new ArrayList<Integer>();
+    	
+    	int[][] Nameww = {{1,2,3,4,},{5,6,7,8}};    //Multi Dimension array declaration
+    	String[][] ancvd= {{"A","B","C","D"},{"E","F","G","H"}};*/
+   
+	}
+}
